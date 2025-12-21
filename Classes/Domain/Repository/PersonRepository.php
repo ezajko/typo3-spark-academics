be97270af674ea9b8362e65276df681f446d5b72
Ernedin Zajko <ezajko@root.ba>
2025-12-21 13:57:59 +0100

    PersonRepository: ignore storage page restriction to fetch records from all PIDs

--- Classes/Domain/Repository/PersonRepository.php
+++ Classes/Domain/Repository/PersonRepository.php
@@ -11,4 +11,10 @@ use TYPO3\CMS\Extbase\Persistence\Repository;
  */
 class PersonRepository extends Repository
 {
+    public function initializeObject(): void
+    {
+        $querySettings = $this->createQuery()->getQuerySettings();
+        $querySettings->setRespectStoragePage(false);
+        $this->setDefaultQuerySettings($querySettings);
+    }
 }

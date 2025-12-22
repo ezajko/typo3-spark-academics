76ff2011a80e83a7188c971b43f9cc675690b5c0
Ernedin Zajko <ezajko@root.ba>
2025-12-22 09:31:14 +0100

    Implement tabbed UI in Academic Profile module using DocHeader menus

--- Classes/Controller/Backend/PersonController.php
+++ Classes/Controller/Backend/PersonController.php
@@ -5,26 +5,30 @@ declare(strict_types=1);
 namespace EtfUnsa\SparkAcademics\Controller\Backend;
 
 use EtfUnsa\SparkAcademics\Domain\Repository\PersonRepository;
-use TYPO3\CMS\Backend\Routing\UriBuilder;
-use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
-use TYPO3\CMS\Core\Context\Context;
-use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Backend\Routing\UriBuilder;
+use TYPO3\CMS\Backend\Template\Components\Menu\MenuRegistry;
+use TYPO3\CMS\Backend\Template\ModuleTemplate;
+use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class PersonController extends ActionController
 {
     protected PersonRepository $personRepository;
     protected ModuleTemplateFactory $moduleTemplateFactory;
     protected UriBuilder $backendUriBuilder;
+    protected MenuRegistry $menuRegistry;
 
     public function __construct(
         PersonRepository $personRepository,
         ModuleTemplateFactory $moduleTemplateFactory,
-        UriBuilder $backendUriBuilder
+        UriBuilder $backendUriBuilder,
+        MenuRegistry $menuRegistry
     ) {
         $this->personRepository = $personRepository;
         $this->moduleTemplateFactory = $moduleTemplateFactory;
         $this->backendUriBuilder = $backendUriBuilder;
+        $this->menuRegistry = $menuRegistry;
     }
 
     public function listAction(): ResponseInterface
@@ -32,17 +36,17 @@ class PersonController extends ActionController
         $currentBeUser = $this->getCurrentBeUser();
         $persons = $this->personRepository->findAll();
         
-        $filteredPersons = [];
+        $userPersons = [];
+        $firstPerson = null;
+
         foreach ($persons as $person) {
             foreach ($person->getBeUsers() as $beUser) {
                 if ($beUser->getUid() === $currentBeUser['uid']) {
-                    // Find all localized versions of this person
-                    // We can use the Repository or a direct query to find records 
-                    // where l10n_parent = $person->getUid()
-                    $uidsToEdit = [$person->getUid()];
+                    if ($firstPerson === null) {
+                        $firstPerson = $person;
+                    }
                     
-                    // Simple manual fetching of translations for the edit link
-                    // In a more robust setup, this would be part of a Service
+                    $uidsToEdit = [$person->getUid()];
                     $query = $this->personRepository->createQuery();
                     $query->getQuerySettings()->setRespectSysLanguage(false);
                     $query->getQuerySettings()->setRespectStoragePage(false);
@@ -53,7 +57,6 @@ class PersonController extends ActionController
                         $uidsToEdit[] = $translation->getUid();
                     }
 
-                    // Generate edit link with multiple UIDs if translations exist
                     $returnUrl = (string)$this->backendUriBuilder->buildUriFromRoute('spark_academics_person');
                     $editUrl = (string)$this->backendUriBuilder->buildUriFromRoute('record_edit', [
                         'edit' => [
@@ -67,7 +70,7 @@ class PersonController extends ActionController
                         'returnUrl' => $returnUrl
                     ]);
                     
-                    $filteredPersons[] = [
+                    $userPersons[] = [
                         'item' => $person,
                         'editUrl' => $editUrl
                     ];
@@ -77,8 +80,37 @@ class PersonController extends ActionController
         }
 
         $moduleTemplate = $this->moduleTemplateFactory->create($this->request);
-        $moduleTemplate->assign('persons', $filteredPersons);
+        $this->registerDocHeaderMenu($moduleTemplate, $this->request->getQueryParams()['tab'] ?? 'overview');
+        
+        $moduleTemplate->assign('persons', $userPersons);
+        $moduleTemplate->assign('firstPerson', $firstPerson);
+        $moduleTemplate->assign('currentTab', $this->request->getQueryParams()['tab'] ?? 'overview');
+
         return $moduleTemplate->renderResponse('Backend/Person/List');
+    }
+
+    protected function registerDocHeaderMenu(ModuleTemplate $moduleTemplate, string $currentTab): void
+    {
+        $menu = $this->menuRegistry->makeMenu();
+        $menu->setIdentifier('spark_academics_person_menu');
+
+        $actions = [
+            'overview' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.tabs.general',
+            'biography' => 'LLL:EXT:spark_academics/Resources/Private/Language/locallang.xlf:person.biography',
+            'contact' => 'LLL:EXT:spark_academics/Resources/Private/Language/locallang.xlf:person.tab.contact',
+            'profiles' => 'LLL:EXT:spark_academics/Resources/Private/Language/locallang.xlf:person.tab.profiles',
+            'media' => 'LLL:EXT:spark_academics/Resources/Private/Language/locallang.xlf:person.tab.media',
+        ];
+
+        foreach ($actions as $action => $label) {
+            $item = $menu->makeMenuItem()
+                ->setTitle($label)
+                ->setHref((string)$this->backendUriBuilder->buildUriFromRoute('spark_academics_person', ['tab' => $action]))
+                ->setActive($currentTab === $action);
+            $menu->addMenuItem($item);
+        }
+
+        $moduleTemplate->getDocHeader()->getMenuRegistry()->addMenu($menu);
     }
 
     protected function getCurrentBeUser(): array

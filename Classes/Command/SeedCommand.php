d6000bf65f0e3958164dcd41982f36eaef3d4699
Ernedin Zajko <ezajko@root.ba>
2025-12-31 08:58:38 +0100

    fix: correct seed data - English primary, proper abbreviations
    - Changed all seed data to use English as primary language (for TYPO3 localization)
    - Fixed AcademicRank abbreviations: Prof., Assoc. Prof., etc. (without 'dr.')
    - Fixed AcademicTitle abbreviations: Dr. prefix, PhD/MSc/BSc suffix
    - Removed title_en from seed data (unnecessary, use TYPO3 localization)
    - ScientificFields, ProjectStatus, ProjectTypes, FundingPrograms all in English
    
    Author: Ernedin Zajko <ezajko@root.ba>

--- Classes/Command/SeedCommand.php
+++ Classes/Command/SeedCommand.php
@@ -169,46 +169,46 @@ class SeedCommand extends Command
         $io->section('Seeding Scientific Fields (OECD FOS)');
         
         $data = [
-            // Level 1 - Major fields
-            ['code' => '1', 'title' => 'Prirodne nauke', 'title_en' => 'Natural Sciences', 'level' => 1],
-            ['code' => '2', 'title' => 'Inženjerstvo i tehnologija', 'title_en' => 'Engineering and Technology', 'level' => 1],
-            ['code' => '3', 'title' => 'Medicinske i zdravstvene nauke', 'title_en' => 'Medical and Health Sciences', 'level' => 1],
-            ['code' => '4', 'title' => 'Poljoprivredne i veterinarske nauke', 'title_en' => 'Agricultural and Veterinary Sciences', 'level' => 1],
-            ['code' => '5', 'title' => 'Društvene nauke', 'title_en' => 'Social Sciences', 'level' => 1],
-            ['code' => '6', 'title' => 'Humanističke nauke i umjetnost', 'title_en' => 'Humanities and the Arts', 'level' => 1],
+            // Level 1 - Major fields (English primary, TYPO3 localization for translations)
+            ['code' => '1', 'title' => 'Natural Sciences', 'level' => 1],
+            ['code' => '2', 'title' => 'Engineering and Technology', 'level' => 1],
+            ['code' => '3', 'title' => 'Medical and Health Sciences', 'level' => 1],
+            ['code' => '4', 'title' => 'Agricultural and Veterinary Sciences', 'level' => 1],
+            ['code' => '5', 'title' => 'Social Sciences', 'level' => 1],
+            ['code' => '6', 'title' => 'Humanities and the Arts', 'level' => 1],
             
-            // Level 2 - Minor fields (Natural Sciences)
-            ['code' => '1.1', 'title' => 'Matematika', 'title_en' => 'Mathematics', 'level' => 2],
-            ['code' => '1.2', 'title' => 'Računarske i informacijske nauke', 'title_en' => 'Computer and Information Sciences', 'level' => 2],
-            ['code' => '1.3', 'title' => 'Fizičke nauke', 'title_en' => 'Physical Sciences', 'level' => 2],
-            ['code' => '1.4', 'title' => 'Hemijske nauke', 'title_en' => 'Chemical Sciences', 'level' => 2],
-            ['code' => '1.5', 'title' => 'Nauke o Zemlji i okolišu', 'title_en' => 'Earth and Related Environmental Sciences', 'level' => 2],
-            ['code' => '1.6', 'title' => 'Biološke nauke', 'title_en' => 'Biological Sciences', 'level' => 2],
-            ['code' => '1.7', 'title' => 'Ostale prirodne nauke', 'title_en' => 'Other Natural Sciences', 'level' => 2],
+            // Level 2 - Natural Sciences
+            ['code' => '1.1', 'title' => 'Mathematics', 'level' => 2],
+            ['code' => '1.2', 'title' => 'Computer and Information Sciences', 'level' => 2],
+            ['code' => '1.3', 'title' => 'Physical Sciences', 'level' => 2],
+            ['code' => '1.4', 'title' => 'Chemical Sciences', 'level' => 2],
+            ['code' => '1.5', 'title' => 'Earth and Related Environmental Sciences', 'level' => 2],
+            ['code' => '1.6', 'title' => 'Biological Sciences', 'level' => 2],
+            ['code' => '1.7', 'title' => 'Other Natural Sciences', 'level' => 2],
             
             // Level 2 - Engineering and Technology
-            ['code' => '2.1', 'title' => 'Građevinarstvo', 'title_en' => 'Civil Engineering', 'level' => 2],
-            ['code' => '2.2', 'title' => 'Elektrotehnika, elektronika i informacijske tehnologije', 'title_en' => 'Electrical, Electronic and Information Engineering', 'level' => 2],
-            ['code' => '2.3', 'title' => 'Mašinstvo', 'title_en' => 'Mechanical Engineering', 'level' => 2],
-            ['code' => '2.4', 'title' => 'Hemijsko inženjerstvo', 'title_en' => 'Chemical Engineering', 'level' => 2],
-            ['code' => '2.5', 'title' => 'Inženjerstvo materijala', 'title_en' => 'Materials Engineering', 'level' => 2],
-            ['code' => '2.6', 'title' => 'Medicinsko inženjerstvo', 'title_en' => 'Medical Engineering', 'level' => 2],
-            ['code' => '2.7', 'title' => 'Inženjerstvo okoliša', 'title_en' => 'Environmental Engineering', 'level' => 2],
-            ['code' => '2.8', 'title' => 'Biotehnologija u okolišu', 'title_en' => 'Environmental Biotechnology', 'level' => 2],
-            ['code' => '2.9', 'title' => 'Industrijska biotehnologija', 'title_en' => 'Industrial Biotechnology', 'level' => 2],
-            ['code' => '2.10', 'title' => 'Nanotehnologija', 'title_en' => 'Nanotechnology', 'level' => 2],
-            ['code' => '2.11', 'title' => 'Ostalo inženjerstvo i tehnologije', 'title_en' => 'Other Engineering and Technologies', 'level' => 2],
+            ['code' => '2.1', 'title' => 'Civil Engineering', 'level' => 2],
+            ['code' => '2.2', 'title' => 'Electrical, Electronic and Information Engineering', 'level' => 2],
+            ['code' => '2.3', 'title' => 'Mechanical Engineering', 'level' => 2],
+            ['code' => '2.4', 'title' => 'Chemical Engineering', 'level' => 2],
+            ['code' => '2.5', 'title' => 'Materials Engineering', 'level' => 2],
+            ['code' => '2.6', 'title' => 'Medical Engineering', 'level' => 2],
+            ['code' => '2.7', 'title' => 'Environmental Engineering', 'level' => 2],
+            ['code' => '2.8', 'title' => 'Environmental Biotechnology', 'level' => 2],
+            ['code' => '2.9', 'title' => 'Industrial Biotechnology', 'level' => 2],
+            ['code' => '2.10', 'title' => 'Nanotechnology', 'level' => 2],
+            ['code' => '2.11', 'title' => 'Other Engineering and Technologies', 'level' => 2],
             
             // Level 2 - Social Sciences
-            ['code' => '5.1', 'title' => 'Psihologija', 'title_en' => 'Psychology', 'level' => 2],
-            ['code' => '5.2', 'title' => 'Ekonomija i biznis', 'title_en' => 'Economics and Business', 'level' => 2],
-            ['code' => '5.3', 'title' => 'Obrazovne nauke', 'title_en' => 'Educational Sciences', 'level' => 2],
-            ['code' => '5.4', 'title' => 'Sociologija', 'title_en' => 'Sociology', 'level' => 2],
-            ['code' => '5.5', 'title' => 'Pravo', 'title_en' => 'Law', 'level' => 2],
-            ['code' => '5.6', 'title' => 'Političke nauke', 'title_en' => 'Political Science', 'level' => 2],
-            ['code' => '5.7', 'title' => 'Socijalna i ekonomska geografija', 'title_en' => 'Social and Economic Geography', 'level' => 2],
-            ['code' => '5.8', 'title' => 'Mediji i komunikacije', 'title_en' => 'Media and Communications', 'level' => 2],
-            ['code' => '5.9', 'title' => 'Ostale društvene nauke', 'title_en' => 'Other Social Sciences', 'level' => 2],
+            ['code' => '5.1', 'title' => 'Psychology', 'level' => 2],
+            ['code' => '5.2', 'title' => 'Economics and Business', 'level' => 2],
+            ['code' => '5.3', 'title' => 'Educational Sciences', 'level' => 2],
+            ['code' => '5.4', 'title' => 'Sociology', 'level' => 2],
+            ['code' => '5.5', 'title' => 'Law', 'level' => 2],
+            ['code' => '5.6', 'title' => 'Political Science', 'level' => 2],
+            ['code' => '5.7', 'title' => 'Social and Economic Geography', 'level' => 2],
+            ['code' => '5.8', 'title' => 'Media and Communications', 'level' => 2],
+            ['code' => '5.9', 'title' => 'Other Social Sciences', 'level' => 2],
         ];
         
         return $this->insertRecords($io, 'tx_spark_scientific_field', $data, $pid, $force, 'code');
@@ -221,18 +221,19 @@ class SeedCommand extends Command
     {
         $io->section('Seeding Academic Ranks');
         
+        // English primary, abbreviations are rank only (title like Dr. comes from AcademicTitle)
         $data = [
-            ['title' => 'Redovni profesor', 'abbreviation' => 'prof. dr.', 'title_en' => 'Full Professor', 'sorting' => 10],
-            ['title' => 'Vanredni profesor', 'abbreviation' => 'v.prof. dr.', 'title_en' => 'Associate Professor', 'sorting' => 20],
-            ['title' => 'Docent', 'abbreviation' => 'doc. dr.', 'title_en' => 'Assistant Professor', 'sorting' => 30],
-            ['title' => 'Viši asistent', 'abbreviation' => 'v.asist.', 'title_en' => 'Senior Assistant', 'sorting' => 40],
-            ['title' => 'Asistent', 'abbreviation' => 'asist.', 'title_en' => 'Assistant', 'sorting' => 50],
-            ['title' => 'Profesor emeritus', 'abbreviation' => 'prof. emeritus', 'title_en' => 'Professor Emeritus', 'sorting' => 5],
-            ['title' => 'Lektor', 'abbreviation' => 'lekt.', 'title_en' => 'Lecturer', 'sorting' => 60],
-            ['title' => 'Viši lektor', 'abbreviation' => 'v.lekt.', 'title_en' => 'Senior Lecturer', 'sorting' => 55],
-            ['title' => 'Naučni saradnik', 'abbreviation' => 'n.sar.', 'title_en' => 'Research Associate', 'sorting' => 70],
-            ['title' => 'Viši naučni saradnik', 'abbreviation' => 'v.n.sar.', 'title_en' => 'Senior Research Associate', 'sorting' => 65],
-            ['title' => 'Naučni savjetnik', 'abbreviation' => 'n.savj.', 'title_en' => 'Research Advisor', 'sorting' => 15],
+            ['title' => 'Full Professor', 'abbreviation' => 'Prof.', 'sorting' => 10],
+            ['title' => 'Associate Professor', 'abbreviation' => 'Assoc. Prof.', 'sorting' => 20],
+            ['title' => 'Assistant Professor', 'abbreviation' => 'Asst. Prof.', 'sorting' => 30],
+            ['title' => 'Senior Teaching Assistant', 'abbreviation' => 'Sr. TA', 'sorting' => 40],
+            ['title' => 'Teaching Assistant', 'abbreviation' => 'TA', 'sorting' => 50],
+            ['title' => 'Professor Emeritus', 'abbreviation' => 'Prof. Em.', 'sorting' => 5],
+            ['title' => 'Lecturer', 'abbreviation' => 'Lect.', 'sorting' => 60],
+            ['title' => 'Senior Lecturer', 'abbreviation' => 'Sr. Lect.', 'sorting' => 55],
+            ['title' => 'Research Associate', 'abbreviation' => 'Res. Assoc.', 'sorting' => 70],
+            ['title' => 'Senior Research Associate', 'abbreviation' => 'Sr. Res. Assoc.', 'sorting' => 65],
+            ['title' => 'Research Advisor', 'abbreviation' => 'Res. Adv.', 'sorting' => 15],
         ];
         
         return $this->insertRecords($io, 'tx_spark_academic_rank', $data, $pid, $force, 'title');
@@ -245,15 +246,16 @@ class SeedCommand extends Command
     {
         $io->section('Seeding Academic Titles');
         
+        // English primary, abbreviation = before name, abbreviation_after = after name
         $data = [
-            ['title' => 'Doktor nauka', 'abbreviation' => 'Dr.', 'abbreviation_after' => 'PhD', 'title_en' => 'Doctor of Philosophy', 'sorting' => 10],
-            ['title' => 'Magistar nauka', 'abbreviation' => 'Mr.', 'abbreviation_after' => 'MSc', 'title_en' => 'Master of Science', 'sorting' => 20],
-            ['title' => 'Magistar', 'abbreviation' => 'MA', 'abbreviation_after' => 'MA', 'title_en' => 'Master of Arts', 'sorting' => 25],
-            ['title' => 'Diplomirani inženjer', 'abbreviation' => 'Dipl.ing.', 'abbreviation_after' => 'BSc', 'title_en' => 'Bachelor of Science (Engineering)', 'sorting' => 30],
-            ['title' => 'Bachelor', 'abbreviation' => '', 'abbreviation_after' => 'BSc', 'title_en' => 'Bachelor of Science', 'sorting' => 35],
-            ['title' => 'Bachelor of Arts', 'abbreviation' => '', 'abbreviation_after' => 'BA', 'title_en' => 'Bachelor of Arts', 'sorting' => 40],
-            ['title' => 'Doktor medicine', 'abbreviation' => 'Dr. med.', 'abbreviation_after' => 'MD', 'title_en' => 'Doctor of Medicine', 'sorting' => 15],
-            ['title' => 'Akademik', 'abbreviation' => 'Akad.', 'abbreviation_after' => '', 'title_en' => 'Academician', 'sorting' => 5],
+            ['title' => 'Doctor of Philosophy', 'abbreviation' => 'Dr.', 'abbreviation_after' => 'PhD', 'sorting' => 10],
+            ['title' => 'Master of Science', 'abbreviation' => '', 'abbreviation_after' => 'MSc', 'sorting' => 20],
+            ['title' => 'Master of Arts', 'abbreviation' => '', 'abbreviation_after' => 'MA', 'sorting' => 25],
+            ['title' => 'Bachelor of Science', 'abbreviation' => '', 'abbreviation_after' => 'BSc', 'sorting' => 30],
+            ['title' => 'Bachelor of Arts', 'abbreviation' => '', 'abbreviation_after' => 'BA', 'sorting' => 35],
+            ['title' => 'Bachelor of Engineering', 'abbreviation' => '', 'abbreviation_after' => 'BEng', 'sorting' => 32],
+            ['title' => 'Doctor of Medicine', 'abbreviation' => 'Dr.', 'abbreviation_after' => 'MD', 'sorting' => 15],
+            ['title' => 'Academician', 'abbreviation' => 'Acad.', 'abbreviation_after' => '', 'sorting' => 5],
         ];
         
         return $this->insertRecords($io, 'tx_spark_academic_title', $data, $pid, $force, 'title');
@@ -267,13 +269,13 @@ class SeedCommand extends Command
         $io->section('Seeding Project Status');
         
         $data = [
-            ['title' => 'U pripremi', 'color' => '#6c757d', 'description' => 'Project is being prepared'],
-            ['title' => 'Apliciran', 'color' => '#17a2b8', 'description' => 'Application submitted'],
-            ['title' => 'Odobren', 'color' => '#28a745', 'description' => 'Project approved'],
-            ['title' => 'Aktivan', 'color' => '#007bff', 'description' => 'Project is active/running'],
-            ['title' => 'Završen', 'color' => '#6f42c1', 'description' => 'Project completed'],
-            ['title' => 'Odbijen', 'color' => '#dc3545', 'description' => 'Application rejected'],
-            ['title' => 'Suspendovan', 'color' => '#fd7e14', 'description' => 'Project suspended'],
+            ['title' => 'In Preparation', 'color' => '#6c757d', 'description' => 'Project is being prepared'],
+            ['title' => 'Submitted', 'color' => '#17a2b8', 'description' => 'Application submitted'],
+            ['title' => 'Approved', 'color' => '#28a745', 'description' => 'Project approved'],
+            ['title' => 'Active', 'color' => '#007bff', 'description' => 'Project is active/running'],
+            ['title' => 'Completed', 'color' => '#6f42c1', 'description' => 'Project completed'],
+            ['title' => 'Rejected', 'color' => '#dc3545', 'description' => 'Application rejected'],
+            ['title' => 'Suspended', 'color' => '#fd7e14', 'description' => 'Project suspended'],
         ];
         
         return $this->insertRecords($io, 'tx_spark_project_status', $data, $pid, $force, 'title');
@@ -287,13 +289,13 @@ class SeedCommand extends Command
         $io->section('Seeding Project Types');
         
         $data = [
-            ['title' => 'Istraživački projekt', 'color' => '#007bff', 'description' => 'Research-focused project'],
-            ['title' => 'Razvojni projekt', 'color' => '#28a745', 'description' => 'Development project'],
-            ['title' => 'Infrastrukturni projekt', 'color' => '#6c757d', 'description' => 'Infrastructure project'],
-            ['title' => 'Obrazovni projekt', 'color' => '#17a2b8', 'description' => 'Educational/training project'],
-            ['title' => 'Mobilnost', 'color' => '#fd7e14', 'description' => 'Mobility/exchange project'],
-            ['title' => 'Kapacitet izgradnje', 'color' => '#6f42c1', 'description' => 'Capacity building project'],
-            ['title' => 'Inovacijski projekt', 'color' => '#20c997', 'description' => 'Innovation project'],
+            ['title' => 'Research Project', 'color' => '#007bff', 'description' => 'Research-focused project'],
+            ['title' => 'Development Project', 'color' => '#28a745', 'description' => 'Development project'],
+            ['title' => 'Infrastructure Project', 'color' => '#6c757d', 'description' => 'Infrastructure project'],
+            ['title' => 'Educational Project', 'color' => '#17a2b8', 'description' => 'Educational/training project'],
+            ['title' => 'Mobility', 'color' => '#fd7e14', 'description' => 'Mobility/exchange project'],
+            ['title' => 'Capacity Building', 'color' => '#6f42c1', 'description' => 'Capacity building project'],
+            ['title' => 'Innovation Project', 'color' => '#20c997', 'description' => 'Innovation project'],
         ];
         
         return $this->insertRecords($io, 'tx_spark_project_type', $data, $pid, $force, 'title');
@@ -315,9 +317,9 @@ class SeedCommand extends Command
             ['title' => 'DAAD', 'description' => 'German Academic Exchange Service'],
             ['title' => 'Fulbright', 'description' => 'US educational exchange program'],
             ['title' => 'Swiss NSF', 'description' => 'Swiss National Science Foundation'],
-            ['title' => 'Federalno ministarstvo obrazovanja', 'description' => 'Federal Ministry of Education (Bosnia and Herzegovina)'],
-            ['title' => 'Kantonalno ministarstvo obrazovanja', 'description' => 'Cantonal Ministry of Education'],
-            ['title' => 'Vlastita sredstva', 'description' => 'Internal funding / Self-funded'],
+            ['title' => 'Federal Ministry', 'description' => 'Federal Ministry of Education funding'],
+            ['title' => 'Cantonal Ministry', 'description' => 'Cantonal Ministry of Education funding'],
+            ['title' => 'Internal Funding', 'description' => 'Internal / Self-funded'],
         ];
         
         return $this->insertRecords($io, 'tx_spark_funding_program', $data, $pid, $force, 'title');

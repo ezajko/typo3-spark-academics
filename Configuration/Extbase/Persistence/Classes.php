dd57bb93e330e4a5de12732b4e082ec53e3f96fa
Ernedin Zajko <ezajko@root.ba>
2026-01-05 17:38:00 +0100

    fix(spark-academics): Fix Extbase mapping and add course generator
    - Corrected `Classes.php` with comprehensive mapping for all entities including `SDG`, `PersonEducation`, etc.
    - Added `GenerateTestCoursesCommand` for future test data generation.
    
    Author: Ernedin Zajko <ezajko@root.ba>

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -3,14 +3,70 @@
 declare(strict_types=1);
 
 return [
+    // --- Core Entities ---
+    \EtfUnsa\SparkAcademics\Domain\Model\Person::class => [
+        'tableName' => 'tx_spark_person',
+    ],
+    \EtfUnsa\SparkAcademics\Domain\Model\PersonEducation::class => [
+        'tableName' => 'tx_spark_person_education',
+    ],
+    \EtfUnsa\SparkAcademics\Domain\Model\PersonMentoring::class => [
+        'tableName' => 'tx_spark_person_mentoring',
+    ],
+    \EtfUnsa\SparkAcademics\Domain\Model\Department::class => [
+        'tableName' => 'tx_spark_department',
+    ],
+    \EtfUnsa\SparkAcademics\Domain\Model\Chair::class => [
+        'tableName' => 'tx_spark_chair',
+    ],
+    \EtfUnsa\SparkAcademics\Domain\Model\ResearchLab::class => [
+        'tableName' => 'tx_spark_research_lab',
+    ],
+    \EtfUnsa\SparkAcademics\Domain\Model\ResearchGroup::class => [
+        'tableName' => 'tx_spark_research_group',
+    ],
+    \EtfUnsa\SparkAcademics\Domain\Model\Project::class => [
+        'tableName' => 'tx_spark_project',
+    ],
+    \EtfUnsa\SparkAcademics\Domain\Model\Partner::class => [
+        'tableName' => 'tx_spark_partner',
+    ],
+
+    // --- Course Entities ---
     \EtfUnsa\SparkAcademics\Domain\Model\Course::class => [
         'tableName' => 'tx_spark_course',
     ],
     \EtfUnsa\SparkAcademics\Domain\Model\CourseSyllabus::class => [
         'tableName' => 'tx_spark_course_syllabus',
     ],
-    \EtfUnsa\SparkAcademics\Domain\Model\Sdg::class => [
+    \EtfUnsa\SparkAcademics\Domain\Model\ExternalCourse::class => [
+        'tableName' => 'tx_spark_external_course',
+    ],
+    \EtfUnsa\SparkAcademics\Domain\Model\StudyProgram::class => [
+        'tableName' => 'tx_spark_study_program',
+    ],
+
+    // --- Lookup Tables ---
+    \EtfUnsa\SparkAcademics\Domain\Model\SDG::class => [
         'tableName' => 'tx_spark_sdg',
+    ],
+    \EtfUnsa\SparkAcademics\Domain\Model\ScientificField::class => [
+        'tableName' => 'tx_spark_scientific_field',
+    ],
+    \EtfUnsa\SparkAcademics\Domain\Model\AcademicRank::class => [
+        'tableName' => 'tx_spark_academic_rank',
+    ],
+    \EtfUnsa\SparkAcademics\Domain\Model\AcademicTitle::class => [
+        'tableName' => 'tx_spark_academic_title',
+    ],
+    \EtfUnsa\SparkAcademics\Domain\Model\FundingProgram::class => [
+        'tableName' => 'tx_spark_funding_program',
+    ],
+    \EtfUnsa\SparkAcademics\Domain\Model\ProjectStatus::class => [
+        'tableName' => 'tx_spark_project_status',
+    ],
+    \EtfUnsa\SparkAcademics\Domain\Model\ProjectType::class => [
+        'tableName' => 'tx_spark_project_type',
     ],
     \EtfUnsa\SparkAcademics\Domain\Model\CourseCategory::class => [
         'tableName' => 'tx_spark_course_category',
@@ -26,31 +82,5 @@ return [
     ],
     \EtfUnsa\SparkAcademics\Domain\Model\Language::class => [
         'tableName' => 'tx_spark_language',
-    ],
-    \EtfUnsa\SparkAcademics\Domain\Model\ExternalCourse::class => [
-        'tableName' => 'tx_spark_external_course',
-    ],
-    
-    // Existing models mapping (just in case they are missing too)
-    \EtfUnsa\SparkAcademics\Domain\Model\Department::class => [
-        'tableName' => 'tx_spark_department',
-    ],
-    \EtfUnsa\SparkAcademics\Domain\Model\Chair::class => [
-        'tableName' => 'tx_spark_chair',
-    ],
-    \EtfUnsa\SparkAcademics\Domain\Model\ResearchLab::class => [
-        'tableName' => 'tx_spark_research_lab',
-    ],
-    \EtfUnsa\SparkAcademics\Domain\Model\ResearchGroup::class => [
-        'tableName' => 'tx_spark_research_group',
-    ],
-    \EtfUnsa\SparkAcademics\Domain\Model\Person::class => [
-        'tableName' => 'tx_spark_person',
-    ],
-    \EtfUnsa\SparkAcademics\Domain\Model\Project::class => [
-        'tableName' => 'tx_spark_project',
-    ],
-    \EtfUnsa\SparkAcademics\Domain\Model\ScientificField::class => [
-        'tableName' => 'tx_spark_scientific_field',
     ],
 ];

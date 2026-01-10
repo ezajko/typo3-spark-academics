93fd12c0447c552e66c47c7ae0756669fb814434
Ernedin Zajko <ezajko@root.ba>
2026-01-10 13:49:25 +0100

    refactor(academics): normalize FlexForm settings keys to camelCase

--- Classes/Service/DemandService.php
+++ Classes/Service/DemandService.php
@@ -76,9 +76,9 @@ class DemandService
     protected function applyStudyProgramFilters(Demand $demand, array $settings): void
     {
         $departmentUid = (int)($settings['filter']['department'] ?? $settings['filter.department'] ?? $settings['filter_department'] ?? 0);
-        $typeUid = (int)($settings['filter']['study_type'] ?? $settings['filter.study_type'] ?? $settings['filter_study_type'] ?? 0);
-        $modeUid = (int)($settings['filter']['mode_of_study'] ?? $settings['filter.mode_of_study'] ?? $settings['filter_mode_of_study'] ?? 0);
-        $languageUid = (int)($settings['filter']['language'] ?? $settings['filter.language'] ?? $settings['filter_language'] ?? 0);
+        $typeUid = (int)($settings['filter']['studyType'] ?? $settings['filter']['study_type'] ?? $settings['filter.study_type'] ?? 0);
+        $modeUid = (int)($settings['filter']['modeOfStudy'] ?? $settings['filter']['mode_of_study'] ?? $settings['filter.mode_of_study'] ?? 0);
+        $languageUid = (int)($settings['filter']['language'] ?? $settings['filter.language'] ?? 0);
 
         // Filter by Department
         if ($departmentUid > 0) {
@@ -106,11 +106,11 @@ class DemandService
 
     protected function applyCourseFilters(Demand $demand, array $settings): void
     {
-        $departmentUid = (int)($settings['filter']['department'] ?? $settings['filter.department'] ?? $settings['filter_department'] ?? 0);
-        $chairUid = (int)($settings['filter']['chair'] ?? $settings['filter.chair'] ?? $settings['filter_chair'] ?? 0);
-        $cycleUid = (int)($settings['filter']['study_cycle'] ?? $settings['filter.study_cycle'] ?? $settings['filter_study_cycle'] ?? 0);
-        $categoryUid = (int)($settings['filter']['course_category'] ?? $settings['filter.course_category'] ?? $settings['filter_course_category'] ?? 0);
-        $fieldUid = (int)($settings['filter']['scientific_field'] ?? $settings['filter.scientific_field'] ?? $settings['filter_scientific_field'] ?? 0);
+        $departmentUid = (int)($settings['filter']['department'] ?? $settings['filter.department'] ?? 0);
+        $chairUid = (int)($settings['filter']['chair'] ?? $settings['filter.chair'] ?? 0);
+        $cycleUid = (int)($settings['filter']['studyCycle'] ?? $settings['filter']['study_cycle'] ?? $settings['filter.study_cycle'] ?? 0);
+        $categoryUid = (int)($settings['filter']['courseCategory'] ?? $settings['filter']['course_category'] ?? $settings['filter.course_category'] ?? 0);
+        $fieldUid = (int)($settings['filter']['scientificField'] ?? $settings['filter']['scientific_field'] ?? $settings['filter.scientific_field'] ?? 0);
         
         // Filter by Department
         if ($departmentUid > 0) {
@@ -146,13 +146,13 @@ class DemandService
 
     protected function applyPersonFilters(Demand $demand, array $settings): void
     {
-        $primaryDepartmentUid = (int)($settings['filter']['primary_department'] ?? $settings['filter.primary_department'] ?? $settings['filter_primary_department'] ?? 0);
-        $departmentUid = (int)($settings['filter']['department'] ?? $settings['filter.department'] ?? $settings['filter_department'] ?? 0);
-        $academicRankUid = (int)($settings['filter']['academic_rank'] ?? $settings['filter.academic_rank'] ?? $settings['filter_academic_rank'] ?? 0);
-        $academicTitleUid = (int)($settings['filter']['academic_title'] ?? $settings['filter.academic_title'] ?? $settings['filter_academic_title'] ?? 0);
-        $groupUid = (int)($settings['filter']['research_group'] ?? $settings['filter.research_group'] ?? $settings['filter_research_group'] ?? 0);
-        $labUid = (int)($settings['filter']['research_lab'] ?? $settings['filter.research_lab'] ?? $settings['filter_research_lab'] ?? 0);
-        $chairUid = (int)($settings['filter']['chair'] ?? $settings['filter.chair'] ?? $settings['filter_chair'] ?? 0);
+        $primaryDepartmentUid = (int)($settings['filter']['primaryDepartment'] ?? $settings['filter']['primary_department'] ?? $settings['filter.primary_department'] ?? 0);
+        $departmentUid = (int)($settings['filter']['department'] ?? $settings['filter.department'] ?? 0);
+        $academicRankUid = (int)($settings['filter']['academicRank'] ?? $settings['filter']['academic_rank'] ?? $settings['filter.academic_rank'] ?? 0);
+        $academicTitleUid = (int)($settings['filter']['academicTitle'] ?? $settings['filter']['academic_title'] ?? $settings['filter.academic_title'] ?? 0);
+        $groupUid = (int)($settings['filter']['researchGroup'] ?? $settings['filter']['research_group'] ?? $settings['filter.research_group'] ?? 0);
+        $labUid = (int)($settings['filter']['researchLab'] ?? $settings['filter']['research_lab'] ?? $settings['filter.research_lab'] ?? 0);
+        $chairUid = (int)($settings['filter']['chair'] ?? $settings['filter.chair'] ?? 0);
 
         if ($primaryDepartmentUid > 0) {
             $primaryDepartment = $this->departmentRepository->findBy(['uid' => $primaryDepartmentUid])->getFirst();

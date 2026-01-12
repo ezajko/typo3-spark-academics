9979c9d16578f847d65c87616ef4d57427565cfc
Ernedin Zajko <ezajko@root.ba>
2026-01-12 15:13:08 +0100

    fix(academics): update OrganizationController to use OrganizationDemand DTO
    - Replace deprecated generic Demand class with entity-specific OrganizationDemand
    - Use proper setType() and setParent() setters instead of generic addFilter()
    - Add PHPDoc to findItems() method
    
    Author: Ernedin Zajko <ezajko@root.ba>

---
+++ Classes/Controller/Backend/OrganizationController.php
@@ -0,0 +1,112 @@
+<?php
+
+/*
+ * This file is part of the "Spark Academics" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * (c) Ernedin Zajko <ezajko@root.ba>
+ */
+
+declare(strict_types=1);
+
+namespace EtfUnsa\SparkAcademics\Controller\Backend;
+
+use EtfUnsa\SparkAcademics\Domain\Repository\OrganizationRepository;
+use EtfUnsa\SparkAcademics\Domain\Repository\OrganizationTypeRepository;
+use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
+use TYPO3\CMS\Backend\Routing\UriBuilder;
+
+/**
+ * Backend controller for Organization entities
+ * 
+ * @author Ernedin Zajko <ezajko@root.ba>
+ */
+class OrganizationController extends AbstractBackendController
+{
+    protected ?OrganizationRepository $organizationRepository = null;
+    protected ?OrganizationTypeRepository $organizationTypeRepository = null;
+    protected \EtfUnsa\SparkCore\Service\BackendPermissionService $backendPermissionService;
+    protected string $tableName = 'tx_spark_organization';
+
+    public function __construct(
+        ModuleTemplateFactory $moduleTemplateFactory,
+        UriBuilder $backendUriBuilder,
+        OrganizationTypeRepository $organizationTypeRepository
+    ) {
+        parent::__construct($moduleTemplateFactory, $backendUriBuilder);
+        $this->organizationTypeRepository = $organizationTypeRepository;
+    }
+
+    public function injectOrganizationRepository(OrganizationRepository $organizationRepository): void
+    {
+        $this->organizationRepository = $organizationRepository;
+        $this->repository = $organizationRepository;
+    }
+
+    public function injectBackendPermissionService(\EtfUnsa\SparkCore\Service\BackendPermissionService $backendPermissionService): void
+    {
+        $this->backendPermissionService = $backendPermissionService;
+    }
+
+    protected function getTemplatePath(): string
+    {
+        return 'Backend/Organization/List';
+    }
+
+    /**
+     * Find organization items based on current filter
+     * 
+     * @param array $currentBeUser Current backend user data
+     * @return iterable Organization entities
+     */
+    protected function findItems(array $currentBeUser)
+    {
+        $filter = $this->request->hasArgument('filter') 
+            ? $this->request->getArgument('filter') 
+            : [];
+
+        // Create OrganizationDemand and apply filters
+        $demand = new \EtfUnsa\SparkAcademics\Domain\Model\Dto\OrganizationDemand();
+        
+        if (!empty($filter['search'])) {
+            $demand->setSearch($filter['search']);
+        }
+        if (!empty($filter['type'])) {
+            $demand->setType((int)$filter['type']);
+        }
+        if (!empty($filter['parent'])) {
+            $demand->setParent((int)$filter['parent']);
+        }
+        
+        // Use findByDemand if any filters are active, otherwise findAll
+        if ($demand->hasSearch() || $demand->hasFilters()) {
+            return $this->organizationRepository->findByDemand($demand);
+        }
+
+        return $this->organizationRepository->findAll();
+    }
+
+    public function listAction(): ResponseInterface
+    {
+        // Get filter value
+        $filter = $this->request->hasArgument('filter') 
+            ? $this->request->getArgument('filter') 
+            : [];
+
+        // Permission check for "new record" button
+        $canCreate = $this->backendPermissionService->hasPermission('spark_perm_org_groups');
+        
+        // Assign filter options
+        $this->additionalViewVariables = [
+            'canCreate' => $canCreate,
+            'filter' => $filter,
+            'availableTypes' => $this->organizationTypeRepository->findAll(),
+            'availableParents' => $this->organizationRepository->findAll(),
+        ];
+        
+        return parent::listAction();
+    }
+}
